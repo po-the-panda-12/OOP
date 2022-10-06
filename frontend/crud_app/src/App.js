1c51c4560b87bc06282525001154e3f7f3f59d3b
piplupOwo <82926705+piplupOwo@users.noreply.github.com>
2022-10-06 15:36:51 +0800

    Frontend React test

---
+++ frontend/crud_app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Create from './components/create';
+
+function App() {
+  return (
+    <div className="main">
+      <h2 className="main-header">Create User for Corporate Pass Application</h2>
+      <div>
+        <Create/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
