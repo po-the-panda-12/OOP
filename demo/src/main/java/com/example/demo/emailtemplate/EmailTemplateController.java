4054820d48bacc79aadb798fba633d6f57bb0f85
Setsuna Yukiii <siang-meng@live.com>
2022-10-25 11:38:31 +0800

    backend for email templates

---
+++ demo/src/main/java/com/example/demo/emailtemplate/EmailTemplateController.java
@@ -0,0 +1,49 @@
+package com.example.demo.emailtemplate;
+
+import com.example.demo.loanpass.Loanpass;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "api/v1/emailTemplates")
+
+public class EmailTemplateController {
+    private final EmailTemplateService emailTemplateService;
+    @Autowired
+    public EmailTemplateController(EmailTemplateService emailTemplateService){
+        this.emailTemplateService = emailTemplateService;
+    }
+    @GetMapping
+    public List<EmailTemplate> getEmailTemplates(){
+        return emailTemplateService.getEmailTemplates();
+    }
+
+    @GetMapping(path="/get/{emailTemplateId}")
+    public Optional<EmailTemplate> getEmailTemplateById(@PathVariable("emailTemplateId") Integer emailTemplateId){
+        return emailTemplateService.getEmailTemplateById(emailTemplateId);
+    }
+
+    @PostMapping
+    public void addEmailTemplate(@RequestBody EmailTemplate emailTemplate) {
+        emailTemplateService.addNewEmailTemplate(emailTemplate);
+    }
+
+    @DeleteMapping(path="{emailTemplateId}")
+    public void deleteEmailTemplate(@PathVariable("emailTemplateId") Integer emailTemplateId){
+        emailTemplateService.deleteEmailTemplate(emailTemplateId);
+    }
+
+    @PutMapping(path="{emailTemplateId}")
+    public void updateEmailTemplate(
+            @PathVariable("emailTemplateId") Integer emailTemplateId,
+            @RequestParam(required = true) String emailTemplateName,
+            @RequestParam(required = true) String emailTemplateBody
+    ){
+        emailTemplateService.updateEmailRepository(emailTemplateId,emailTemplateName,emailTemplateBody);
+    }
+
+}
---
+++ demo/src/main/java/com/example/demo/emailtemplate/EmailTemplateService.java
@@ -0,0 +1,57 @@
+package com.example.demo.emailtemplate;
+import com.example.demo.loanpass.Loanpass;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class EmailTemplateService {
+    private final EmailTemplateRepository emailTemplateRepository;
+
+    @Autowired
+    public EmailTemplateService(EmailTemplateRepository emailTemplateRepository){
+        this.emailTemplateRepository = emailTemplateRepository;
+    }
+
+    public List<EmailTemplate> getEmailTemplates(){
+        return emailTemplateRepository.findAll();
+    }
+
+    public Optional<EmailTemplate> getEmailTemplateById(Integer emailTemplateId){
+        return emailTemplateRepository.getEmailTempalteById(emailTemplateId);
+    }
+
+    public void addNewEmailTemplate(EmailTemplate emailTemplate){
+//        Optional<EmailTemplate> emailTemplateOptional = emailTemplateRepository.findEmailTemplateById(emailTemplate.getEmailTemplateId());
+//        if(emailTemplateOptional.isPresent()){
+//            throw new IllegalStateException()
+//        }
+        emailTemplateRepository.save(emailTemplate);
+    }
+    public void deleteEmailTemplate(int emailTemplateId){
+        boolean exists = emailTemplateRepository.existsById(emailTemplateId);
+        if(!exists){
+            throw new IllegalStateException("Email Template with id " + emailTemplateId + " does not exists");
+        }
+        emailTemplateRepository.deleteById(emailTemplateId);
+    }
+
+    @Transactional
+    public void updateEmailRepository(int emailTemplateId, String emailTemplateName, String emailTemplateBody)
+    {
+        EmailTemplate emailTemplate = emailTemplateRepository.findById(emailTemplateId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Email Template with id " + emailTemplateId + "does not exist"
+                ));
+        if (emailTemplateName != null && emailTemplateName.length() > 0 && !Objects.equals(emailTemplate.getEmailTemplateName(), emailTemplateName)){
+            emailTemplate.setEmailTemplateName(emailTemplateName);
+        }
+        if (emailTemplateBody != null && emailTemplateBody.length() > 0 && !Objects.equals(emailTemplate.getEmailTemplateBody(), emailTemplateBody)){
+            emailTemplate.setEmailTemplateBody(emailTemplateBody);
+        }
+    }
+}
