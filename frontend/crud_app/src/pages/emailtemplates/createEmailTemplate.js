b64259c0f62141aebe865f54c0741e056138cf0f
Setsuna Yukiii <siang-meng@live.com>
2022-10-27 12:58:01 +0800

    restructuring folders

---
+++ frontend/crud_app/src/pages/emailtemplates/createEmailTemplate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import EmailTemplateForm from '../../components/emailtemplates/emailTemplateForm';
+import { Link } from 'react-router-dom';
+function CreateEmailTemplate() {
+    return (
+        <div>
+            <h2>Create template</h2>
+            <EmailTemplateForm/>
+            <button class = "btn btn-secondary">
+                <Link to = "/react/emailtemplates" style = {{color:"white"}}>
+                    Cancel
+                </Link>
+            </button>
+        </div>
+    );
+}
+
+export default CreateEmailTemplate;
