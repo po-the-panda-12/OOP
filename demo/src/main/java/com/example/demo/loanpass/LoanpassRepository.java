b923dd9301e9d003f6c57eb36c7ec8142efd04b1
piplupOwo <82926705+piplupOwo@users.noreply.github.com>
2022-10-30 13:59:10 +0800

    updated get all loanpass backend to return all loanpasses in an order based on pass_id

--- demo/src/main/java/com/example/demo/loanpass/LoanpassRepository.java
+++ demo/src/main/java/com/example/demo/loanpass/LoanpassRepository.java
@@ -16,4 +16,8 @@ public interface LoanpassRepository extends JpaRepository<Loanpass, Integer> {
 
     @Query("SELECT s FROM Loanpass s WHERE s.attractionId = ?1")
     List<Loanpass> findLoanPassByAttractionId(Integer attractionId);
+
+    // select all from loanpass order by pass id
+    @Query("SELECT s FROM Loanpass s ORDER BY s.passId")
+    List<Loanpass> findAllLoanPassOrdered();
 }
