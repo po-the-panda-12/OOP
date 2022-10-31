ad0d14a708a80200c28bc71480ca029317b8c3c6
Setsuna Yukiii <siang-meng@live.com>
2022-10-31 18:23:06 +0800

    fixing some compilation and runtime errors i guess?

--- demo/src/main/java/com/example/demo/successloan/SuccessloanController.java
+++ demo/src/main/java/com/example/demo/successloan/SuccessloanController.java
@@ -1,7 +1,5 @@
 package com.example.demo.successloan;
 
-import com.example.demo.users.Users;
-import com.example.demo.users.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
--- demo/src/main/java/com/example/demo/uploadCsv/UploadController.java
+++ demo/src/main/java/com/example/demo/uploadCsv/UploadController.java
@@ -1,6 +1,6 @@
 package com.example.demo.uploadCsv;
 
-import com.example.demo.users.Users;
+import com.example.demo.users.AppUser;
 import com.univocity.parsers.csv.CsvParser;
 import com.univocity.parsers.csv.CsvParserSettings;
 import com.univocity.parsers.common.record.Record;
@@ -24,26 +24,26 @@ public class UploadController {
 
     @PostMapping
     public String uploadCSV(@RequestParam("file") MultipartFile file) throws Exception{
-        List<Users> userList = new ArrayList<>();
+        List<AppUser> userList = new ArrayList<>();
         InputStream inputStream = file.getInputStream();
         CsvParserSettings settings = new CsvParserSettings();
         settings.setHeaderExtractionEnabled(true);
         CsvParser parser = new CsvParser(settings);
         List<Record> parseAllRecords = parser.parseAllRecords(inputStream);
         parseAllRecords.forEach(record ->{
-            Users user = new Users();
+            AppUser user = new AppUser();
             user.setPassword("defaultPassword");
             user.setEmail(record.getString("Email"));
-            user.setName(record.getString("Name"));
+            user.setUsername(record.getString("Name"));
             userList.add(user);
         });
         String addedUser = "";
         String skippedUser = "";
-        for(Users u : userList){
+        for(AppUser u : userList){
             if(service.addUser(u)){
-                addedUser += "Added User " + u.getName() + "\n";
+                addedUser += "Added User " + u.getUsername() + "\n";
             }else{
-                skippedUser += "Skipped User " + u.getName() + "\n";
+                skippedUser += "Skipped User " + u.getUsername() + "\n";
             }
         }
         return addedUser + skippedUser;
