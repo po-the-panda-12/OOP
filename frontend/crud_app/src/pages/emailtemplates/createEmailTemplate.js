8e02a5923919803c863805f8462b6fc4e1954c4e
Setsuna Yukiii <siang-meng@live.com>
2022-11-05 09:30:35 +0800

    beautifying the UI

--- frontend/crud_app/src/pages/emailtemplates/createEmailTemplate.js
+++ frontend/crud_app/src/pages/emailtemplates/createEmailTemplate.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function CreateEmailTemplate() {
     return (
         <div class="container rounded content">
-            <div class="card"style={{ width: '25rem', height: '50vh'}}>
+            <div class="card"style={{ width: '60rem'}}>
                 <h2 class="main-header">Create template</h2>
                 <EmailTemplateForm template={null} />
                 <button class="btn btn-secondary btn-sm">
