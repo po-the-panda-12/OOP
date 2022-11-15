08226bdf683689c5e54b2204387234a0e0229a15
piplupOwo <82926705+piplupOwo@users.noreply.github.com>
2022-11-15 17:37:53 +0800

    add me as user so i dont have to keep signing up

--- demo/src/main/java/com/example/demo/users/UsersConfig.java
+++ demo/src/main/java/com/example/demo/users/UsersConfig.java
@@ -15,11 +15,13 @@ public class UsersConfig {
             userService.saveRole(new UserRole(null, "ROLE_ADMIN"));
             userService.saveRole(new UserRole(null, "ROLE_OFFICER"));
 
+            userService.saveUser(new AppUser(null, "Adam", "adam334123@gmail.com", "12345678", "91234567", true, new ArrayList<>(), null));
             userService.saveUser(new AppUser(null, "Brian Lim", "brianlimjj@gmail.com", "12345678", "12345678",true, new ArrayList<>(),null ));
             userService.saveUser(new AppUser(null, "John", "johm@gmail.com", "12345678", "12345678",true, new ArrayList<>(),null ));
             userService.saveUser(new AppUser(null, "Ted", "ted@gmail.com", "12345678", "12345678",true, new ArrayList<>(),null ));
             userService.saveUser(new AppUser(null, "Siang Meng", "sm.lee.2020@smu.edu.sg", "12345678", "12345678",true, new ArrayList<>(),null ));
 
+            userService.addRoleToUser("Adam", "ROLE_ADMIN");
             userService.addRoleToUser("Brian Lim", "ROLE_USER");
             userService.addRoleToUser("Siang Meng", "ROLE_USER");
             userService.addRoleToUser("John", "ROLE_ADMIN");
